817de6514d835f8e4a8b779f28116ed5734eec0c
ambidextrousTx <ravisinha@my.unt.edu>
2014-11-11 23:26:42 -0600

    Delete stub

--- src/test.js
+++
@@ -1,9 +0,0 @@
-// testing if Node actually does work asynchronously
-//
-fs = require('fs')
-console.log('first');
-fs.readFile('test.js', function(error, data) {
-    console.log(data);
-});
-console.log('second');
-console.log('third');
