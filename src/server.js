0807ea2a919d9fdf33f52a7278e618e881e3a9f8
ambidextrousTx <ravisinha@my.unt.edu>
2014-11-18 23:09:20 -0600

    Move to express; move to templating using ejs; first render

--- src/server.js
+++ src/server.js
@@ -2,24 +2,18 @@
 // and manages requests to fetch links from 
 // a page
 
-var http = require('http');
+var express = require('express');
 var host = 'localhost';
 var port = 1337;
 
-var server = http.createServer(function(request, response) {
-    console.log('Request received: ', request.url);
-    if (request.url == '/test') {
-        response.writeHead(200, {'Content-Type': 'text/plain'});
-        response.write('Hold your horses, got your request!');
-    } else {
-        response.writeHead(404, {'Content-Type': 'text/plain'});
-        response.write('Sorry, this URL is not valid or not supported\n');
-        response.end('Please try again.');
-    }
+var app = express.createServer();
 
+app.get('/', function(request, response) {
+    response.render('index.ejs', {title: 'All is fine and dandy'});
 });
 
-server.listen(port, host, function() {
-    console.log('Listening on port:', port);
-
+app.get('/*', function(request, response) {
+    response.render('error.ejs', {title: 'Uh oh, something went awry'});
 });
+
+app.listen(port, host);
